1d4cdd1a69f53d3644755a8fb3d3ea95dde22b3e
Antoine Rybacki <antoine.rybacki@etu.univ-lyon1.fr>
2020-12-20 16:07:31 +0100

    first commit

---
+++ lennyslash/mycog.py
@@ -0,0 +1,212 @@
+import discord
+from redbot.core.bot import Red
+from redbot.core import commands
+from discord_slash import SlashCommand
+from discord_slash import SlashContext
+import aiohttp
+import asyncio
+import logging
+import random
+from typing import Dict, List
+
+__author__ = "tmerc, Lifeismana"
+
+log = logging.getLogger("red.Lifeismana.lenny")
+
+
+LENNY_PARTS: Dict[str, List[str]] = {
+    "ears": [
+        "q{}p",
+        "ʢ{}ʡ",
+        "⸮{}?",
+        "ʕ{}ʔ",
+        "ᖗ{}ᖘ",
+        "ᕦ{}ᕥ",
+        "ᕦ({})ᕥ",
+        "ᕙ({})ᕗ",
+        "ᘳ{}ᘰ",
+        "ᕮ{}ᕭ",
+        "ᕳ{}ᕲ",
+        "({})",
+        "[{}]",
+        "¯\\\\_{}_/¯",
+        "୧{}୨",
+        "୨{}୧",
+        "⤜({})⤏",
+        "☞{}☞",
+        "ᑫ{}ᑷ",
+        "ᑴ{}ᑷ",
+        "ヽ({})ﾉ",
+        "\\\\({})/",
+        "乁({})ㄏ",
+        "└[{}]┘",
+        "(づ{})づ",
+        "(ง{})ง",
+        "|{}|",
+    ],
+    "eyes": [
+        "⌐■{}■",
+        " ͠°{} °",
+        "⇀{}↼",
+        "´• {} •`",
+        "´{}`",
+        "`{}´",
+        "ó{}ò",
+        "ò{}ó",
+        ">{}<",
+        "Ƹ̵̡ {}Ʒ",
+        "ᗒ{}ᗕ",
+        "⪧{}⪦",
+        "⪦{}⪧",
+        "⪩{}⪨",
+        "⪨{}⪩",
+        "⪰{}⪯",
+        "⫑{}⫒",
+        "⨴{}⨵",
+        "⩿{}⪀",
+        "⩾{}⩽",
+        "⩺{}⩹",
+        "⩹{}⩺",
+        "◥▶{}◀◤",
+        "≋{}≋",
+        "૦ઁ{}૦ઁ",
+        "  ͯ{}  ͯ",
+        "  ̿{}  ̿",
+        "  ͌{}  ͌",
+        "ළ{}ළ",
+        "◉{}◉",
+        "☉{}☉",
+        "・{}・",
+        "▰{}▰",
+        "ᵔ{}ᵔ",
+        "□{}□",
+        "☼{}☼",
+        "*{}*",
+        "⚆{}⚆",
+        "⊜{}⊜",
+        ">{}>",
+        "❍{}❍",
+        "￣{}￣",
+        "─{}─",
+        "✿{}✿",
+        "•{}•",
+        "T{}T",
+        "^{}^",
+        "ⱺ{}ⱺ",
+        "@{}@",
+        "ȍ{}ȍ",
+        "x{}x",
+        "-{}-",
+        "${}$",
+        "Ȍ{}Ȍ",
+        "ʘ{}ʘ",
+        "Ꝋ{}Ꝋ",
+        "๏{}๏",
+        "■{}■",
+        "◕{}◕",
+        "◔{}◔",
+        "✧{}✧",
+        "♥{}♥",
+        " ͡°{} ͡°",
+        "¬{}¬",
+        " º {} º ",
+        "⍜{}⍜",
+        "⍤{}⍤",
+        "ᴗ{}ᴗ",
+        "ಠ{}ಠ",
+        "σ{}σ",
+    ],
+    "mouths": [
+        "v",
+        "ᴥ",
+        "ᗝ",
+        "Ѡ",
+        "ᗜ",
+        "Ꮂ",
+        "ヮ",
+        "╭͜ʖ╮",
+        " ͟ل͜",
+        " ͜ʖ",
+        " ͟ʖ",
+        " ʖ̯",
+        "ω",
+        "³",
+        " ε ",
+        "﹏",
+        "ل͜",
+        "╭╮",
+        "‿‿",
+        "▾",
+        "‸",
+        "Д",
+        "∀",
+        "!",
+        "人",
+        ".",
+        "ロ",
+        "_",
+        "෴",
+        "ѽ",
+        "ഌ",
+        "⏏",
+        "ツ",
+        "益",
+    ],
+}
+
+
+def protect_against_emojification(text) -> str:
+    res = ""
+    for symbol in text:
+        if symbol == "\\":
+            res += symbol
+        else:
+            res += symbol + "\N{VARIATION SELECTOR-15}"
+
+    return res
+
+class Mycog(commands.Cog):
+    """My custom cog"""
+    def __init__(self, bot):
+        self.bot = bot
+        self.slash = SlashCommand(bot, override_type=True)
+
+        self.__url: str = "http://api.lenny.today/v1/random?limit=1"
+        self.__session = aiohttp.ClientSession()
+        # Cog is only supported by commands ext, so just skip checking type.
+        @self.slash.slash(name="lenny")
+        async def lenny(ctx: SlashContext) -> None:
+            """☞⇀‿↼☞"""
+            await ctx.send(send_type=3,content=await self.__get_lenny())
+
+
+    def cog_unload(self) -> None:
+        self.slash.remove()
+        if self.__session:
+            asyncio.get_event_loop().create_task(self.__session.close())
+
+    async def __get_lenny(self) -> str:
+        try:
+            async with self.__session.get(self.__url) as response:
+                # grab the face
+                lenny = (await response.json())[0]["face"]
+                # escape markdown characters
+                lenny = discord.utils.escape_markdown(lenny)
+                # protect against discord transforming symbol in emoji
+                lenny = protect_against_emojification(lenny)
+        # if the API call fails, make a (more limited) lenny locally instead
+        except aiohttp.ClientError:
+            log.warning("API call failed; falling back to local lenny")
+            lenny = (
+                random.choice(LENNY_PARTS["ears"])
+                .format(random.choice(LENNY_PARTS["eyes"]))
+                .format(random.choice(LENNY_PARTS["mouths"]))
+            )
+
+        return lenny            
+
+    async def test(self, ctx: SlashContext):
+        await ctx.send(embeds=[discord.Embed(title="lel a slash command")])
+    """ @self.slash.slash(name="test")
+    async def _test(ctx: SlashContext):
+        await ctx.send(content="Hello, World!") """
